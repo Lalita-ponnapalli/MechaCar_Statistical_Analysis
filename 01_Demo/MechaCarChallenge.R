98d34b70fb77e9cab0c81d3d38a1c5949b1ea629
lalita-ponnapalli <lalita.pn1@gmail.com>
2022-06-10 18:54:49 -0500

    new files

---
+++ 01_Demo/MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dtplyr)
+library(tidyverse)
+mecha_data <- read.csv("MechaCar_mpg.csv",stringsAsFactors = F,check.names = F)
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mecha_data) 
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mecha_data))
